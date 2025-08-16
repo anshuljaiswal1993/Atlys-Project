e09d445617c726049808dbfcc89c70ae80e2f117
Anshul Jaiswal <jaiswal.anshul7@gmail.com>
2025-08-16 15:19:44 +0530

    valid user login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useAuth } from "../../auth/AuthContext";
 import AuthModal from "../../components/AuthModal/AuthModal";
 import styles from "./Feed.module.css";
 
